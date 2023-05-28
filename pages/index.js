efffac068394f1c1e05ec1e89a0e2c7f757b32a4
nighthowler27 <128471906+nighthowler27@users.noreply.github.com>
2023-05-28 14:47:51 +0800

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Header from "@/components/Header";
+import Featured from "@/components/Featured";
+import Footer from "@/components/Footer";
+import Copyright from "@/components/Copyright";
+import { Product } from "@/models/Product";
+import { mongooseConnect } from "@/lib/mongoose copy";
+import NewProducts from "@/components/NewProducts";
+
+export default function HomePage({ featuredProduct, newProducts }) {
+    return (
+        <div>
+            <Header />
+            <Featured product={featuredProduct} />
+            <NewProducts products={newProducts} />
+            <Footer />
+            <Copyright />
+        </div>
+    );
+}
+
+export async function getServerSideProps() {
+    const featuredProductId = '6472e43c7948ff3c5f520b8c';
+    await mongooseConnect();
+    const featuredProduct = await Product.findById(featuredProductId);
+    const newProducts = await Product.find({}, null, { sort: { '_id': -1 }, limit: 10 });
+    return {
+        props: {
+            featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+            newProducts: JSON.parse(JSON.stringify(newProducts)),
+        },
+    };
+}
